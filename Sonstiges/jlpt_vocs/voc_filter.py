e2dc24557782e78ad6568bd474314bf80914855c
Max <maxdepenbrock@me.com>
2016-05-24 07:46:49 +0200

    New syntax for dakuten and improvements on voc_filter.py
    - Removed ':' in kana strings as we slice anyway
    - Added output before terminating with illegal number of arguments
    - Implemented new syntax for dakuten which allows to be more precise in the selection of desired chars

--- Sonstiges/jlpt_vocs/voc_filter.py
+++ Sonstiges/jlpt_vocs/voc_filter.py
@@ -4,29 +4,35 @@ import sys
 import re
 
 # arg1 = gyou n ∈ {0,1,...,10}
-# arg2 = dakuten (Y/N)
+# arg2 = flags: d = dakuten; s = sokuon; y = youon; c = chouon; a = all
 # arg3 = maximale JLPT-Level
 # hiragana katakana offset: 96
 
 arg_count = len(sys.argv)
 if arg_count != 5:
+  print("Error: Wrong number of arguments")
   exit()
 
 gyou = int(sys.argv[1])
-dakuten = sys.argv[2]
+flags = sys.argv[2]
 level = int(sys.argv[3])
 output = sys.argv[4]
 
-# indeces 0-10 (but off by one)
-kana_list = ["あいうえお", "かきくけこ:がぎぐげご", "さしすせそ:ざじずぜぞ", "たちつてと:だぢづでど", "なにぬねの", "はひふへほ:ばびぶべぼぱぴぷぺぽ", "まみむめも", "やゆよ", "らりるれろ", "わをん", "っゃゅょー"]
+# indeces 0-9 (but off by one)
+kana_list = ["あいうえお", "かきくけこがぎぐげご", "さしすせそざじずぜぞ", "たちつてとだぢづでど", "なにぬねの", "はひふへほばびぶべぼぱぴぷぺぽ", "まみむめも", "やゆよ", "らりるれろ", "わをん"]
 
 kana_list = kana_list[:gyou]
 chars = "" # string containing allowed chars
 for elem in kana_list:
-  if not (dakuten == "Y" or dakuten == "y" or dakuten == "yes"):
-    if ":" in elem:
-      elem = elem[:5]
+  if not ("d" in flags or "a" in flags):
+    elem = elem[:5]
   chars += elem
+  if "s" in flags or "a" in flags:
+    chars += "っ"
+  if "y" in flags or "a" in flags:
+    chars += "ゃゅょ"
+  if "c" in flags or "a" in flags:
+    chars += "ー"
 
 vocab_list = []
 reading_list = []
